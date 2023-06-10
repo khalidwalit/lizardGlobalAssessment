ba7cf6ba033a8ca5f6ee764a6ef820817a32caa9
Khalid Walit <harithkhalid93@gmail.com>
2023-06-11 02:01:24 +0800

    1- Retrieve the data from the mock API.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
 function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/posts')
+      .then((res) => res.json())
+      .then((json) => setPosts(json.posts));
+  }, []);
+  console.log(posts);
+
+  return <div></div>;
 }
 
 export default App;
